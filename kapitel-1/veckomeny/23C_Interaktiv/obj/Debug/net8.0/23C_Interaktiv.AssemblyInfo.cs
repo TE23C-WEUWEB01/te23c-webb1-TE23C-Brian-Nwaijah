a879efd642d0c858695795c3c1072c3cf72c45b3
Brian Nwaijah <brian.nwaijah@elev.ga.ntig.se>
2024-09-20 09:43:50 +0200

    vi har tränat på div boxen och

--- kapitel-1/veckomeny/23C_Interaktiv/obj/Debug/net8.0/23C_Interaktiv.AssemblyInfo.cs
+++ kapitel-1/veckomeny/23C_Interaktiv/obj/Debug/net8.0/23C_Interaktiv.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("23C_Interaktiv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aa950903a6ae26a75a85f03aaf648d551cc928d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69e9995e22866a50b5d48880928787400cf691cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("23C_Interaktiv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("23C_Interaktiv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
