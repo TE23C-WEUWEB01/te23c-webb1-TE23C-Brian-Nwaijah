50c157b707b92c231a5df7b60cb92c30258ed4b5
Brian Nwaijah <brian.nwaijah@elev.ga.ntig.se>
2024-11-07 14:25:57 +0100

    Vi har tränat på arbetsgång i webb

--- kapitel-1/veckomeny/23C_Interaktiv/obj/Debug/net8.0/23C_Interaktiv.AssemblyInfo.cs
+++ kapitel-1/veckomeny/23C_Interaktiv/obj/Debug/net8.0/23C_Interaktiv.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("23C_Interaktiv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15f1cbc421f2b97c346848db2140f75e21588fd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e6d09254e095bfa0a99621ae4e0f318cd300b50")]
 [assembly: System.Reflection.AssemblyProductAttribute("23C_Interaktiv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("23C_Interaktiv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
